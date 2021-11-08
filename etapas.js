e43b2d83e7e505f0dfecefb8dbd958b908130af4
dev-Alencar <lucalencar.dev@gmail.com>
2021-11-08 11:20:59 -0300

    estrutura doc js

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Fulano de Tal',
+                partido: 'ABD',
+                fotos: [
+                    {url: '3811.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.png', legenda: 'vereador'}
+                ]
+            }
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [ 
+                    {url:'99.png', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QUERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: '84.png', legenda: 'Prefeito'},
+                    {url: '84_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
